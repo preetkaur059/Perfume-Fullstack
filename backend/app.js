aa52f13c6ae479ef20cb031361955010923c3d9d
Parampreet Kaur <parampreetkaur059@gmail.com>
2026-09-26 11:16:25 +0530

    refactor app.js

--- backend/app.js
+++ backend/app.js
@@ -1,35 +1,34 @@
-import express, { json } from "express"
-import dotenv from "dotenv"
-import userRouter from "./routes/userRouter.js"
-import productsRouter from "./routes/productsRouter.js"
-import orderRouter from "./routes/orderRouter.js"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express, { json } from "express";
+import dotenv from "dotenv";
+import userRouter from "./routes/userRouter.js";
+import productsRouter from "./routes/productsRouter.js";
+import orderRouter from "./routes/orderRouter.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import adminOrderRoutes from "./routes/adminOrderRoutes.js";
 
-    dotenv.config()
+dotenv.config();
 
-    const app = express();
+const app = express();
 
-    app.use(cors({
-        origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
-        credentials: true,
-    }))
+app.use(
+  cors({
+    origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
-    app.use(json())
-    app.use(cookieParser())
+app.use(json());
+app.use(cookieParser());
 
-    app.use("/users", userRouter)
-    app.use("/products", productsRouter)
-    app.use("/orders", orderRouter)
-    
-    app.use("/admin/orders", adminOrderRoutes);
+app.use("/users", userRouter);
+app.use("/products", productsRouter);
+app.use("/orders", orderRouter);
 
-    app.get("/", (req, res)=>{
-    res.json("You server is active")
-    })
+app.use("/admin/orders", adminOrderRoutes);
 
-
-
+app.get("/", (req, res) => {
+  res.json("You server is active");
+});
 
 export default app;
